c9fdf643d7d9b7eaa641a0b580e6ef849772de0e
MauRRR <mrdevs@outlook.es>
2026-06-08 21:58:06 -0600

    add lint, daisy, tailwind, and configs

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-7xl font-bold text-cyan-900 ">TRAVEL JOURNEY</h1>
+    <button class = "btn btn-secondary">damn son <Icon name="tabler:map" /> </button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  css: ['~/assets/css/main.css'],
+  eslint: {
+    config: {
+      standalone: false
+    }
+  },
+  vite:{
+    plugins: [tailwindcss()]
+  }
 })
