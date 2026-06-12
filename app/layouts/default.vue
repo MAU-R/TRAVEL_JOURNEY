34b6aa30c40fb2373b976276dca31d3ad258fd4e
MauRRR <mrdevs@outlook.es>
2026-06-12 14:50:08 -0600

    sidebar_sidebarpersist_ssrauth

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,8 +5,17 @@
 <template>
   <div class="flex fmin-h-screen flex-col">
     <NavBar />
-    <main>
+    <main
+      class="flex-1 flex flex-col"
+    >
       <slot />
     </main>
   </div>
 </template>
+
+<script lang="ts"
+      setup
+    >
+const authStore = useAuthStore();
+await authStore.init();
+</script>
